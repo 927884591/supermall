4e80e1bd4376c6185d7f362729f604ba5af0a928
927884591 <927884591@qq.com>
2022-01-18 17:24:30 +0800

    添加了banner

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,17 +3,32 @@
     <nav-bar>
       <div slot="center">购物街</div>
     </nav-bar>
+    <HomeBanner :banner="banner" :recommend="recommend"></HomeBanner>
   </div>
 </template>
 
 <script>
 import NavBar from "../../components/common/navbar/NavBar.vue";
+import HomeBanner from "./childComponents/HomeBanner.vue"
+import {getMultidata} from "../../network/home.js"
 export default {
   name: "Home",
+  data(){
+    return{
+      banner:[],
+      recommend:[]
+    }
+  },
   components: {
     NavBar,
-    // NavBar,
+    HomeBanner
   },
+  created(){
+    getMultidata().then(res =>{
+      this.banner = res.data.banner.list
+      this.recommend = res.data.recommend.list
+    })
+  }
 };
 </script>
 
---
+++ src/views/home/childComponents/HomeBanner.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="banner">
+        <Swiper>
+            <SwiperItem v-for="item in banner">
+                <a :href="item.link">
+                    <img :src="item.image" alt="">
+                </a>
+            </SwiperItem>
+        </Swiper>
+    </div>
+</template>
+<script>
+import { Swiper, SwiperItem } from '../../../components/common/swiper'
+export default {
+    name:"HomeBanner",
+    props:{
+        banner:{
+            type:Array,
+            default(){
+                return []
+            }
+        },
+        recommend:{
+            type:Array,
+            default(){
+                return []
+            }
+        }
+    },
+    components:{
+        Swiper,
+        SwiperItem
+    }
+}
+</script>
+<style>
+    .banner{
+        margin-top: 44px;
+    }
+</style>
