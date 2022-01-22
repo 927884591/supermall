5e8f33045368180cb8000c024a8825841117498e
927884591 <927884591@qq.com>
2022-01-22 18:20:54 +0800

    防抖

--- src/views/home/childComponents/HomeBanner.vue
+++ src/views/home/childComponents/HomeBanner.vue
@@ -1,40 +1,53 @@
 <template>
-    <div class="banner">
-        <Swiper>
-            <SwiperItem v-for="item in banner">
-                <a :href="item.link">
-                    <img :src="item.image" alt="">
-                </a>
-            </SwiperItem>
-        </Swiper>
-    </div>
+  <div class="banner">
+    <Swiper>
+      <SwiperItem v-for="item in banner">
+        <a :href="item.link">
+          <img :src="item.image" alt="" @load="imageLoad" />
+        </a>
+      </SwiperItem>
+    </Swiper>
+  </div>
 </template>
 <script>
-import { Swiper, SwiperItem } from '../../../components/common/swiper'
+import { Swiper, SwiperItem } from "../../../components/common/swiper";
 export default {
-    name:"HomeBanner",
-    props:{
-        banner:{
-            type:Array,
-            default(){
-                return []
-            }
-        },
-        recommend:{
-            type:Array,
-            default(){
-                return []
-            }
-        }
+  name: "HomeBanner",
+  data() {
+    return {
+      isLoad: false,
+    };
+  },
+  props: {
+    banner: {
+      type: Array,
+      default() {
+        return [];
+      },
     },
-    components:{
-        Swiper,
-        SwiperItem
-    }
-}
+    recommend: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+  methods: {
+    imageLoad() {
+      if (!this.isLoad) {
+        this.$emit("imageload")
+        this.isLoad = true;
+      }
+    },
+  },
+};
 </script>
 <style>
-    .banner{
-        margin-top: 44px;
-    }
+.banner {
+  margin-top: 44px;
+}
 </style>
